a002e410cef1c2ca99aa9038a59cd5da5c0f639e
jihun1844 <108657822+jihun1844@users.noreply.github.com>
2022-07-06 12:16:24 +0900

    Add files via upload

---
+++ 0701수업 다시.py
@@ -0,0 +1,14 @@
+# flow = 5
+# num = 5
+
+# for flowC in range(flow):
+#     for numC in range(num):
+#         if numC == flowC or flowC==0 or flowC==4 :
+#             print("*", end="")
+#         elif numC+ flowC==4:
+#             print("*", end="")
+#         else:
+#             print(" ", end="")
+#     print()
+
+
